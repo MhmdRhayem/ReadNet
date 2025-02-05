8f59fdadc0440a9d07cace728d0561b31d7c981a
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:28:46 +0200

    add findAll method to Genre model for retrieving all unique genres

--- models/Genre.js
+++ models/Genre.js
@@ -1,1 +1,14 @@
 import db from '../config/database.js';
+
+class Genre {
+    static async findAll() {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT DISTINCT genre_id, genre_name FROM Genre';
+            db.query(query, (err, results) => {
+                if (err) reject(err);
+                resolve(results);
+            });
+        });
+    }
+    
+}
