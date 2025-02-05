5b72821313ad21fc014c30bd41cfa6bf793ad9ff
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:27:48 +0200

    add findAll method to Book model for retrieving all book records

--- models/Book.js
+++ models/Book.js
@@ -95,4 +95,14 @@ class Book {
             });
         });
     }
+
+    static async findAll() {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM Book';
+            db.query(query, (err, results) => {
+                if (err) reject(err);
+                resolve(results);
+            });
+        });
+    }
 }
