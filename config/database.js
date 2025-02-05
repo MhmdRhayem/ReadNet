1e9c9d213dee0b1973da65f113e811672c709e7d
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 11:44:25 +0200

    chore: add MySQL connection handling and logging in database configuration

--- config/database.js
+++ config/database.js
@@ -13,3 +13,13 @@ const db = mysql.createConnection({
     password: password,
     database: database,
 });
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL');
+});
+
+export default db; 
