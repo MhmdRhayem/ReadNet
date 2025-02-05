0cfd1ac168410dad13d7d4f657030097e1b9921d
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:44:52 +0200

    add: initialize reader routes with express router

--- routes/readerRoutes.js
+++ routes/readerRoutes.js
@@ -1,3 +1,5 @@
 import express from 'express';
 import { authenticateToken, authorizeRole, verifyUser } from '../middleware/auth.js';
 import * as readerController from '../controllers/readerController.js';
+
+const router = express.Router();
