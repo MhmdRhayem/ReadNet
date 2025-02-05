d8b20fc38a367aa64bc6b8f5a0e02b19b162317d
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:43:11 +0200

    add HTML routes for login and signup pages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,3 +2,7 @@ import express from 'express';
 import * as authController from '../controllers/authController.js';
 
 const router = express.Router();
+
+// HTML Routes (these will be mounted at root '/')
+router.get('/login', authController.getLoginPage);
+router.get('/signup', authController.getSignupPage);
