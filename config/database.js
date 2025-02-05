088a805ac6604be9832fb72b98aecd0069a9da04
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 11:44:03 +0200

    chore: update database configuration to use environment variables for connection details

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
+
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
