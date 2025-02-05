5126cc0bbd998bcdf7abf4ae9e3c3c5749c6547c
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:19:54 +0200

    add getAuthorHome function to serve the author's home HTML page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,3 +3,10 @@ import Book from '../models/Book.js';
 import Genre from '../models/Genre.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Serve static HTML pages
+export const getAuthorHome = async (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'author', 'home.html'));
+};
