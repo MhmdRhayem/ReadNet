b1f893187212f0bd40f10123c01d4f4563033b85
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:20:46 +0200

    add getAuthorBooks function to retrieve books by author and categorize them by genre

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -18,3 +18,23 @@ export const getAddBookPage = async (req, res) => {
 export const getEditBookPage = async (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'author', 'edit.html'));
 };
+
+// API endpoints 
+export const getAuthorBooks = async (req, res) => {
+    try {
+        const authorId = req.params.author_id;
+        const books = await Book.findByAuthorId(authorId);
+        const genres = await Genre.findAll();
+        const booksByGenre = genres.map(genre => {
+            const filteredBooks = books.filter(book => book.genre_id === genre.genre_id);
+            return {
+            genre: genre.genre_name,
+            books: filteredBooks
+            };
+        }).filter(item => item.books.length > 0);
+        res.status(200).json({ booksByGenre });
+    } catch (err) {
+        console.error('Error in getAuthorBooks:', err);
+        res.status(500).json({ message: 'Error getting author books' });
+    }
+};
