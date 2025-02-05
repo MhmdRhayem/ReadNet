65e122d79eb975c54bd7ebddb8ce62beacd7bb9e
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 11:46:01 +0200

    chore: initialize Express app and set up server configuration

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ import authorRoutes from './routes/authorRoutes.js';
 import readerRoutes from './routes/readerRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import generalRoutes from './routes/generalRoutes.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const PORT = 3000;
