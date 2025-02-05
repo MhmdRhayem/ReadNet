579e3661ac5e7305974383d6283599a040c2703c
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:26:19 +0200

    add findById method to Book model for retrieving book details by ID

--- models/Book.js
+++ models/Book.js
@@ -1,1 +1,19 @@
 import db from '../config/database.js';
+
+class Book {
+    static async findById(bookId) {
+        return new Promise((resolve, reject) => {
+            const query = `
+                SELECT Book.*, Genre.genre_name, Author.name as author_name 
+                FROM Book 
+                JOIN Genre ON Book.genre_id = Genre.genre_id 
+                JOIN Author ON Book.author_id = Author.author_id 
+                WHERE Book.book_id = ?
+            `;
+            db.query(query, [bookId], (err, results) => {
+                if (err) reject(err);
+                resolve(results[0]);
+            });
+        });
+    }
+}
