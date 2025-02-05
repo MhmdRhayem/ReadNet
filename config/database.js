c00b7c87183a07de0b329c1c0506eb75aeaf08ab
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 11:44:15 +0200

    chore: establish MySQL connection using environment variables in database configuration

--- config/database.js
+++ config/database.js
@@ -6,3 +6,10 @@ const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE;
+
+const db = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database,
+});
