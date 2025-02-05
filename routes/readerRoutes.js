a729b74e2512e4f9a42848090cdc271b197e94ae
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:44:43 +0200

    add: create reader routes with authentication and authorization middleware

---
+++ routes/readerRoutes.js
@@ -0,0 +1,3 @@
+import express from 'express';
+import { authenticateToken, authorizeRole, verifyUser } from '../middleware/auth.js';
+import * as readerController from '../controllers/readerController.js';
