e4963d8ad66228dc816ea1669f0e0e9c9409cd17
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 11:51:57 +0200

    chore: define __dirname in authController for path resolution

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,3 +2,5 @@ import User from '../models/User.js';
 import { generateToken } from '../middleware/auth.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
