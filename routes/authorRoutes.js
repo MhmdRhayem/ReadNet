4e8de8fdfec6e5ed0dbfdc53aca5827cb5524cf8
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:34:06 +0200

    add multer storage configuration for handling cover page uploads

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -6,3 +6,13 @@ import path from 'path';
 import db from '../config/database.js';
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/cover_pages/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
