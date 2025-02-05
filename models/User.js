4f7139333c5182dda52c415f53676435b68b1c48
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 12:30:27 +0200

    add findAuthorByEmail method to User model for retrieving author by email

--- models/User.js
+++ models/User.js
@@ -1,5 +1,18 @@
 import db from '../config/database.js';
 
 class User {
+    static findAuthorByEmail(email) {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM author WHERE email = ?';
+            db.query(query, [email], (err, results) => { // Changed parameter to 'results'
+                if (err) {
+                    console.error("Database error:", err); // Added error logging
+                    return reject(err);
+                }
+                resolve(results.length > 0 ? results[0] : null);
+            });
+        });
+    }
 
+    
 }
