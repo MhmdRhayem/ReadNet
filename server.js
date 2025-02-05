b44c71f8a2178048fae9b35b6d9c9c8d3c9afc7c
MhmdRhayem <10121011@mu.edu.lb>
2025-02-05 11:45:01 +0200

    chore: add server setup with Express, MySQL, and middleware integration

---
+++ server.js
@@ -0,0 +1,6 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
+import path from "path";
+import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
